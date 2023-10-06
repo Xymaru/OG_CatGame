0f5c5a33091f01ae7c029d152b70042f9f775b74
Victor <victorfalcon796@gmail.com>
2023-10-06 10:58:59 +0200

    sockets

---
+++ Assets/Scripts/LabSessions/Sockets.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LabSessions
+{
+    public class Sockets : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
